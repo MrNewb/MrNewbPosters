ab31bef938871af5e4fc11849444d5c402a8aa9e
Donivan <donivansee@live.com>
2021-12-02 11:18:13 -0500

    init
    init upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+shared_scripts {
+    '@es_extended/imports.lua'
+}
+
+client_scripts {
+    'client_poster.lua'
+}
+
+server_scripts {
+    'server_poster.lua'
+}
+
+dependencies {
+	'es_extended',
+	'mythic_progbar'
+}
