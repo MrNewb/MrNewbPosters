366b5718dfc3765ca4d84fefe1a14eb0d54c2521
Donivan <donivansee@live.com>
2024-02-25 19:03:24 -0500

    event to funct

--- server/server.lua
+++ server/server.lua
@@ -1,39 +1,37 @@
 local cachedData = {}
 
-local function GetCountBySource(src)
+local function getCountBySource(src)
 	return cachedData[src] or 0
 end
 
-local function RemoveDataBySource(src)
+local function removeDataBySource(src)
 	cachedData[src] = nil
+end
+
+local function completeJobPay(src, total)
+	if not total or total < 1 then return end
+	AddMoneyToAccount(src, total)
+	removeDataBySource(src)
 end
 
 RegisterNetEvent("MrNewbPosters:verifycount", function(count)
 	local src = source
 	if not count then return print("Cheater pinging event ID# ", src) end
 	cachedData[src] = (cachedData[src] or 0) + count
-	local total = GetCountBySource(src)
+	local total = getCountBySource(src)
 	if total >= 10 then
-		TriggerEvent("MrNewbPosters:endposterJob", 10)
+		completeJobPay(src, total)
 	end
-	--print(json.encode(GetCountBySource(src)))
 end)
 
 RegisterNetEvent("MrNewbPosters:startjob", function()
 	local src = source
 	local player = src
-    local ped = GetPlayerPed(player)
-    local playerCoords = GetEntityCoords(ped)
+	local ped = GetPlayerPed(player)
+	local playerCoords = GetEntityCoords(ped)
 	local dstCheck = #(playerCoords - Config.StartPosition)
 	if dstCheck <= 9.0 then
 		NotifyPlayer(src, 'Head to the marked location to place a poster.', "inform")
 		TriggerClientEvent("MrNewbPosters:jobstarted", src)
 	end
-end)
-
-RegisterNetEvent("MrNewbPosters:endposterJob", function(total)
-	local src = source
-	if not total or total < 1 then return end
-	AddMoneyToAccount(src, total)
-	RemoveDataBySource(src)
 end)
